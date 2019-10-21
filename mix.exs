575282e2eeb91899132d9efa73262a2abd5d4cfd
Dylan Aspden <dylan@hifyre.com>
2019-10-20 20:09:46 -0400

    feat(analysis): add credo for code analysis

--- mix.exs
+++ mix.exs
@@ -29,6 +29,7 @@ defmodule Transmog.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.10", only: :test}
     ]
