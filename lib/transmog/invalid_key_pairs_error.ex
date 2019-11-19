ea325311780163e4020bfa6f884a1a62477249be
Dylan Aspden <dylan@hifyre.com>
2019-11-19 14:07:51 -0500

    feat(key_pairs): add bang functions for parse and new

---
+++ lib/transmog/invalid_key_pairs_error.ex
@@ -0,0 +1,3 @@
+defmodule Transmog.InvalidKeyPairsError do
+  defexception message: "key pairs are not valid"
+end
---
+++ lib/transmog/invalid_key_path_error.ex
@@ -0,0 +1,3 @@
+defmodule Transmog.InvalidKeyPathError do
+  defexception message: "key path is not valid"
+end
