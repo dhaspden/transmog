deb983b95164dbc98eee77f8297e346251d94580
Dylan Aspden <dylan@hifyre.com>
2019-11-19 21:39:29 -0500

    feat(parser): add bang version `parse!/1`

--- test/transmog/parser/any_test.exs
+++ test/transmog/parser/any_test.exs
@@ -3,12 +3,23 @@ defmodule Transmog.Parser.AnyTest do
 
   use ExUnit.Case, async: true
 
+  alias Transmog.InvalidKeyPathError
   alias Transmog.Parser
 
   describe "parse/1" do
     test "when a value that is not supported is given, then an error is returned" do
       assert {:error, :invalid_key_path} = Parser.parse(nil)
       assert {:error, :invalid_key_path} = Parser.parse(3.14)
+    end
+  end
+
+  describe "parse!/1" do
+    test "when a value that is not supported is given, then an error is raised" do
+      expected = "key path is not valid (nil)"
+
+      assert_raise InvalidKeyPathError, expected, fn ->
+        Parser.parse!(nil)
+      end
     end
   end
 end
--- test/transmog/parser/list_test.exs
+++ test/transmog/parser/list_test.exs
@@ -3,6 +3,7 @@ defmodule Transmog.Parser.ListTest do
 
   use ExUnit.Case, async: true
 
+  alias Transmog.InvalidKeyPathError
   alias Transmog.Parser
 
   describe "parse/1" do
@@ -17,6 +18,25 @@ defmodule Transmog.Parser.ListTest do
 
     test "when the list is empty, then an error is returned" do
       assert {:error, :invalid_key_path} = Parser.parse([])
+    end
+  end
+
+  describe "parse!/1" do
+    test "when the list is not empty, then it is returned as is" do
+      list = [:a, :b]
+      expected = [:a, :b]
+
+      key_path = Parser.parse!(list)
+
+      assert key_path == expected
+    end
+
+    test "when the list is empty, then an error is raised" do
+      expected = "key path is not valid ([])"
+
+      assert_raise InvalidKeyPathError, expected, fn ->
+        Parser.parse!([])
+      end
     end
   end
 end
