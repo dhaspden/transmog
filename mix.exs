c778de40740c7e25dd634968fde470e5021ab08b
Dylan Aspden <dylan@hifyre.com>
2019-12-05 10:42:26 -0500

    fix(library): update earmark to remove version warning

--- mix.exs
+++ mix.exs
@@ -59,6 +59,7 @@ defmodule Transmog.MixProject do
     [
       {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false},
+      {:earmark, "~> 1.3.0", only: :dev, runtime: false},
       {:ex_doc, "~> 0.18.0", only: :dev, runtime: false},
       {:excoveralls, "~> 0.10", only: :test}
     ]
